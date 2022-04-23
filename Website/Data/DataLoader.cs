4243b0ae94d72827eac561bd56aeaf9fda26c83a
cph-sn311 <cph-sn311@cphbusiness.dk>
2022-04-23 20:27:10 +0200

    added basic classes for getting data from mongodb

---
+++ Website/Data/DataLoader.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Website.Data;
+
+public class DataLoader
+{
+    private readonly IMongoCollection<Tweet> _tweetsCollection;
+
+    public DataLoader(IOptions<MongoDbConfiguration> options)
+    {
+        MongoClient client = new(options.Value.ConnectionString);
+        IMongoDatabase db = client.GetDatabase(options.Value.DatabaseName);
+        _tweetsCollection = db.GetCollection<Tweet>(options.Value.CollectionName);
+    }
+    public async Task<List<Tweet>> GetLatest10TweetsAsync()
+        => await _tweetsCollection
+                        .Find(_ => true)
+                        .Limit(10)
+                        .ToListAsync();
+
+    public async Task AddTweetAsync(Tweet tweet)
+        => await _tweetsCollection.InsertOneAsync(tweet);
+}
+
+public class Tweet
+{
+    public string text { get; set; }
+}
+
+public class MongoDbConfiguration
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+    public string CollectionName { get; set; }
+}
--- Website/Program.cs
+++ Website/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MongoDB.Driver;
 using Website.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<DataLoader>();
+
+builder.Services.Configure<MongoDbConfiguration>(builder.Configuration.GetSection("MongoDb"));
 
 var app = builder.Build();
 
